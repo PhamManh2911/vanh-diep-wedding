22f5398f34fd72c29a84fcff1eb25e91ed8b8ff5
PhamManh2911 <phammanh2911@gmail.com>
2023-12-18 02:31:42 +0700

    feat: responsive for timeline session

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,7 +8,7 @@ function VerticalDivider({ disableVertical }) {
   const { isPhone } = useMedia();
 
   return (
-    <Stack sx={{ height: '30%', alignItems: 'center', gap: '-2px' }}>
+    <Stack sx={{ height: 254, alignItems: 'center', gap: '-2px' }}>
       <Image src="/icons/ellipse.svg" alt="ellipse" width={isPhone ? 8 : 16} height={isPhone ? 8 : 16} />
       <Box sx={{
         width: isPhone ? '1px' : '4px',
@@ -27,7 +27,7 @@ function EventIcon({ src, justifyContent }) {
   return (
     <IconImg
       src={src}
-      sx={{ display: 'flex', width: isPhone ? '37.5vw' : '25vw', padding: isPhone ? '4px' : '8px', justifyContent, alignItems: 'center', gap: '8px' }}
+      sx={{ display: 'flex', width: isPhone ? 150 : 420, padding: isPhone ? '4px' : '8px', justifyContent, alignItems: 'center', gap: '8px' }}
       imageSx={{ width: isPhone ? 60 : 120, height: isPhone ? 60 : 120 }}
     />
   );
@@ -38,8 +38,8 @@ function EventContentWrapper({ children, alignItems }) {
 
   return (
     <Stack sx={{
-      width: isPhone ? '37.5vw' : '25vw',
-      padding: isPhone ? '4px' : '0px',
+      width: isPhone ? 150 : 420,
+      padding: isPhone ? '4px' : '8px',
       justifyContent: 'center',
       alignItems,
       gap: isPhone ? '4px' : '16px',
@@ -88,39 +88,35 @@ export function TimelineSession() {
 
   return (
     <Stack
-      sx={{ padding: isPhone ? '16px 0 0' : '64px 460px 0', margin: 'auto', width: isPhone ? '380px' : '100%', height: isPhone ? '100vh' : '100%' }}
+      sx={{ padding: isPhone ? '16px 0 32px' : '64px 0 0', margin: 'auto' }}
       alignItems='center'
       gap={isPhone ? '8px' : '32px'}
       alignSelf='stretch'
     >
-      <Typography variant="headline1" color={theme.palette.primary.dark} textAlign="center" >
-        Lễ thành hôn
-      </Typography>
-      <Typography variant="display" color={theme.palette.primary.dark} textAlign='center'>
-        Ngày mình nên vợ nên chồng
-      </Typography>
+      <Typography variant="headline1" color={theme.palette.primary.dark} textAlign="center" >LỄ THÀNH HÔN</Typography>
+      <Typography variant="display" color={theme.palette.primary.dark} textAlign='center'>Ngày mình nên vợ nên chồng</Typography>
       <Stack alignItems='center'>
-        <Box height= '30%' sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
+        <Box height={isPhone ? 174 : 254} sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
           <EventIcon src="/icons/record.svg" justifyContent="flex-end" />
           <VerticalDivider />
           <EventContentWrapper alignItems="flex-start">
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>09:00 SÁNG</Typography>
+            <Box sx={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
+              <Typography variant="headline2" textAlign='left' color={theme.palette.neutral.dark}>09:00 SÁNG</Typography>
               {isPhone ? null : (<Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>:</Typography>)}
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>ĐÓN KHÁCH</Typography>
+              <Typography variant="headline2" textAlign='left' color={theme.palette.neutral.dark}>ĐÓN KHÁCH</Typography>
             </Box>
             <EventDecs justifyContent="flex-start" textAlign='left'>Sự hiện diện quý giá</EventDecs>
-            <EventMessage alignItems="flex-start">
+            <EventMessage alignItems="flex-start" textAlign='left'>
               Gia đình chúng tôi rất hân hạnh được đón tiếp quý khách trong một ngày trọng đại như thế này! 
             </EventMessage>
           </EventContentWrapper>
         </Box>
-        <Box height= '30%' sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
+        <Box height={isPhone ? 174 : 254} sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
           <EventContentWrapper alignItems="flex-end">
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>09:30 SÁNG</Typography>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
+              <Typography variant="headline2" textAlign='right' color={theme.palette.neutral.dark}>09:30 SÁNG</Typography>
               {isPhone ? null : (<Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>:</Typography>)}
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>LÀM LỄ</Typography>
+              <Typography variant="headline2" textAlign='right' color={theme.palette.neutral.dark}>LÀM LỄ</Typography>
             </Box>
             <EventDecs justifyContent="flex-end" textAlign='right'>Giây phút thiêng liêng</EventDecs>
             <EventMessage alignItems="flex-end" textAlign='right'>
@@ -130,27 +126,27 @@ export function TimelineSession() {
           <VerticalDivider />
           <EventIcon src="/icons/rings.svg" justifyContent="flex-start" />
         </Box>
-        <Box height= '30%' sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
+        <Box height={isPhone ? 174 : 254} sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
           <EventIcon src="/icons/champagne.svg" justifyContent="flex-end" />
           <VerticalDivider />
           <EventContentWrapper alignItems="flex-start">
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>10:30 SÁNG</Typography>
+            <Box sx={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
+              <Typography variant="headline2" textAlign='left' color={theme.palette.neutral.dark}>10:30 SÁNG</Typography>
               {isPhone ? null : (<Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>:</Typography>)}
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>NHẬP TIỆC</Typography>
+              <Typography variant="headline2" textAlign='left' color={theme.palette.neutral.dark}>NHẬP TIỆC</Typography>
             </Box>
             <EventDecs justifyContent="flex-start" textAlign='left'>Thưởng thức ẩm thực</EventDecs>
-            <EventMessage alignItems="flex-start">
+            <EventMessage alignItems="flex-start" textAlign='left'>
               Gia đình kính mời quý khách dùng bữa, nếm trọn hương vị cỗ cưới miền Bắc đặc trưng.
             </EventMessage>
           </EventContentWrapper>
         </Box>
-        <Box height= '30%' sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
+        <Box height={isPhone ? 174 : 254} sx={{ display: 'flex', alignItems: 'flex-start', gap: '8px' }} >
           <EventContentWrapper alignItems="flex-end">
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>12:00 SÁNG</Typography>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end', gap: '8px', flexDirection: isPhone ? 'column' : 'row' }}>
+              <Typography variant="headline2" textAlign='right' color={theme.palette.neutral.dark}>12:00 SÁNG</Typography>
               {isPhone ? null : (<Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>:</Typography>)}
-              <Typography variant="headline2" textAlign='center' color={theme.palette.neutral.dark}>GIAO LƯU VĂN NGHỆ</Typography>
+              <Typography variant="headline2" textAlign='right' color={theme.palette.neutral.dark}>GIAO LƯU VĂN NGHỆ</Typography>
             </Box>
             <EventDecs justifyContent="flex-end" textAlign='left'>Niềm vui bất tận</EventDecs>
             <EventMessage alignItems="flex-end" textAlign='right'>
