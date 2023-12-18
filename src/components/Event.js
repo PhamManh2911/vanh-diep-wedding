3345003c894255a74162216b20267b61510fb304
truongdx <truongdx@nami.trade>
2023-12-18 12:34:57 +0700

    Fix UI follow require by Viet Anh

--- src/components/Event.js
+++ src/components/Event.js
@@ -8,65 +8,187 @@ import { nha } from "@/configs/app";
 export const EventSession = forwardRef(function EventSession(_, ref) {
   const theme = useTheme();
   const { isPhone } = useMedia();
-  const nhaTrai = nha === 'trai';
-
+  const nhaTrai = nha === "trai";
+  const handleDirectMap = () => {
+    const link = nhaTrai
+      ? "https://maps.app.goo.gl/RgeiZyurMFx6y9JA6?g_st=ic"
+      : "https://maps.app.goo.gl/zRJGL7FLHF8JfVu1A?g_st=ic";
+    return window.open(link, "_blank");
+  };
   return (
-    <Stack ref={ref} padding={isPhone ? '16px 0' : '32px 0 64px'} alignItems='center' justifyContent='center' gap={isPhone ? '8px' : '32px'}>
-      <Typography variant="headline1" color={theme.palette.neutral.dark} textAlign='center'>SỰ KIỆN CHÍNH</Typography>
-      <Typography variant="display" color={theme.palette.neutral.dark} textAlign="center">Save The Date</Typography>
-      <Box sx={{ display: 'flex', padding: isPhone ? '0' : '0px 32px', justifyContent: 'center', alignItems: 'center', gap: isPhone ? '16px' : '32px', flexDirection: isPhone ? 'column' : 'row' }}>
-        <Stack padding='8px' justifyContent="center" alignItems='center' gap='16px' width={isPhone ? 202 : 253}>
-          <Typography variant="headline2" color={theme.palette.primary.midDark} textAlign="center">TIỆC CƯỚI</Typography>
-          <Stack justifyContent='center' alignItems='flex-start' gap="8px">
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+    <Stack
+      ref={ref}
+      padding={isPhone ? "16px 0" : "32px 0 64px"}
+      alignItems="center"
+      justifyContent="center"
+      gap={isPhone ? "8px" : "32px"}
+    >
+      <Typography
+        variant="headline1"
+        color={theme.palette.neutral.dark}
+        textAlign="center"
+      >
+        SỰ KIỆN CHÍNH
+      </Typography>
+      <Typography
+        variant="display"
+        color={theme.palette.neutral.dark}
+        textAlign="center"
+      >
+        Save The Date
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          padding: isPhone ? "0" : "0px 32px",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: isPhone ? "16px" : "32px",
+          flexDirection: isPhone ? "column" : "row",
+        }}
+      >
+        <Stack
+          padding="8px"
+          justifyContent="center"
+          alignItems="center"
+          gap="16px"
+          width={isPhone ? 202 : 253}
+        >
+          <Typography
+            variant="headline2"
+            color={theme.palette.primary.midDark}
+            textAlign="center"
+          >
+            TIỆC CƯỚI
+          </Typography>
+          <Stack justifyContent="center" alignItems="flex-start" gap="8px">
+            <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
               <IconImg src="/icons/clock.svg" width={24} height={24} />
-              <Typography variant="body" color='#000' textAlign={isPhone ? 'left' : 'center'}>{nhaTrai ? "16:00 Chiều" : '16:00 Chiều'}</Typography>
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: isPhone ? 'center' : 'flex-start', gap: '8px' }}>
-              <IconImg src="/icons/calendar.svg" width={24} height={24} />
-              <Typography variant="body" color='#000' textAlign={isPhone ? 'left' : 'center'}>
-                Chủ Nhật, 31/12/2023 (19 tháng 11 Âm Lịch)
+              <Typography
+                variant="body"
+                color="#000"
+                textAlign={isPhone ? "left" : "center"}
+              >
+                {nhaTrai ? "16:00 Chiều" : "16:00 Chiều"}
               </Typography>
             </Box>
-            <Box sx={{ display: 'flex', alignItems: isPhone ? 'center' : 'flex-start', gap: '8px' }}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: isPhone ? "center" : "flex-start",
+                gap: "8px",
+              }}
+            >
+              <IconImg src="/icons/calendar.svg" width={24} height={24} />
+              <Typography variant="body" color="#000" textAlign={"left"}>
+                Chủ Nhật, 31/12/2023
+                <br />
+                (19 tháng 11 Âm Lịch)
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: isPhone ? "center" : "flex-start",
+                gap: "8px",
+              }}
+            >
               <IconImg src="/icons/location.svg" width={24} height={24} />
-              <Typography variant="body" color='#000' textAlign={isPhone ? 'left' : 'center'}>
-                {nhaTrai ? 'HỘI TRƯỜNG THPT LƯƠNG TÀI, LƯƠNG TÀI, BẮC NINH' : 'PHÚC BỒI, QUỲNH HƯNG, QUỲNH PHỤ, THÁI BÌNH'}
+              <Typography variant="body" color="#000" textAlign={"left"}>
+                {nhaTrai
+                  ? "HỘI TRƯỜNG THPT LƯƠNG TÀI, LƯƠNG TÀI, BẮC NINH"
+                  : "PHÚC BỒI, QUỲNH HƯNG, QUỲNH PHỤ, THÁI BÌNH"}
               </Typography>
             </Box>
           </Stack>
-          <Button text='Xem địa điểm' />
+          <Button text="Xem địa điểm" onClick={handleDirectMap} />
         </Stack>
-        <Stack width={isPhone ? 240 : 360} height={isPhone ? 240 : 360} justifyContent='flex-end' alignItems='center' sx={{ borderRadius: "50%", background: `url(${isPhone ? "/images/event-phone.png" : "/images/event.png"}), lightgray -170.412px -351.331px / 174.577% 261.833% no-repeat` }}>
+        <Stack
+          width={isPhone ? 240 : 360}
+          height={isPhone ? 240 : 360}
+          justifyContent="flex-end"
+          alignItems="center"
+          sx={{
+            borderRadius: "50%",
+            background: `url(${
+              isPhone ? "/images/event-phone.png" : "/images/event.png"
+            }), lightgray -170.412px -351.331px / 174.577% 261.833% no-repeat`,
+          }}
+        >
           {isPhone ? (
-            <IconImg src="/icons/event-deco-phone.svg" width={289} height={187} sx={{ top: '70px', position: 'relative' }} />
+            <IconImg
+              src="/icons/event-deco-phone.svg"
+              width={289}
+              height={187}
+              sx={{ top: "70px", position: "relative" }}
+            />
           ) : (
-            <IconImg src="/icons/event-deco.svg" width={428} height={270} sx={{ top: '100px', position: 'relative' }} />
+            <IconImg
+              src="/icons/event-deco.svg"
+              width={428}
+              height={270}
+              sx={{ top: "100px", position: "relative" }}
+            />
           )}
         </Stack>
-        <Stack padding='8px' justifyContent="center" alignItems='center' gap='16px' width={isPhone ? 202 : 253} sx={{ marginTop: '20px' }}>
-          <Typography variant="headline2" color={theme.palette.primary.midDark} textAlign="center">{nhaTrai ? 'LỄ THÀNH HÔN' : 'LỄ VU QUY'}</Typography>
-          <Stack justifyContent='center' alignItems='flex-start' gap="8px">
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+        <Stack
+          padding="8px"
+          justifyContent="center"
+          alignItems="center"
+          gap="16px"
+          width={isPhone ? 202 : 253}
+          sx={{ marginTop: "20px" }}
+        >
+          <Typography
+            variant="headline2"
+            color={theme.palette.primary.midDark}
+            textAlign="center"
+          >
+            {nhaTrai ? "LỄ THÀNH HÔN" : "LỄ VU QUY"}
+          </Typography>
+          <Stack justifyContent="center" alignItems="flex-start" gap="8px">
+            <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
               <IconImg src="/icons/clock.svg" width={24} height={24} />
-              <Typography variant="body" color='#000' textAlign={isPhone ? 'left' : 'center'}>{nhaTrai ? '9:30 Sáng' : '07:00 Sáng'}</Typography>
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: isPhone ? 'center' : 'flex-start', gap: '8px' }}>
-              <IconImg src="/icons/calendar.svg" width={24} height={24} />
-              <Typography variant="body" color='#000' textAlign={isPhone ? 'left' : 'center'}>
-                Thứ Hai, 01/01/2024 (20 tháng 11 Âm Lịch)
+              <Typography
+                variant="body"
+                color="#000"
+                textAlign={isPhone ? "left" : "center"}
+              >
+                {nhaTrai ? "9:30 Sáng" : "07:00 Sáng"}
               </Typography>
             </Box>
-            <Box sx={{ display: 'flex', alignItems: isPhone ? 'center' : 'flex-start', gap: '8px' }}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: isPhone ? "center" : "flex-start",
+                gap: "8px",
+              }}
+            >
+              <IconImg src="/icons/calendar.svg" width={24} height={24} />
+              <Typography variant="body" color="#000" textAlign={"left"}>
+                Thứ Hai, 01/01/2024
+                <br />
+                (20 tháng 11 Âm Lịch)
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: isPhone ? "center" : "flex-start",
+                gap: "8px",
+              }}
+            >
               <IconImg src="/icons/location.svg" width={24} height={24} />
-              <Typography variant="body" color='#000' textAlign={isPhone ? 'left' : 'center'}>
-                {nhaTrai ? "HỘI TRƯỜNG THPT LƯƠNG TÀI, LƯƠNG TÀI, BẮC NINH" : 'PHÚC BỒI, QUỲNH HƯNG, QUỲNH PHỤ, THÁI BÌNH'}
+              <Typography variant="body" color="#000" textAlign={"left"}>
+                {nhaTrai
+                  ? "HỘI TRƯỜNG THPT LƯƠNG TÀI, LƯƠNG TÀI, BẮC NINH"
+                  : "PHÚC BỒI, QUỲNH HƯNG, QUỲNH PHỤ, THÁI BÌNH"}
               </Typography>
             </Box>
           </Stack>
-          <Button text='Xem địa điểm' />
+          <Button text="Xem địa điểm" onClick={handleDirectMap} />
         </Stack>
       </Box>
     </Stack>
   );
-})
+});
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography, useTheme } from "@mui/material";
 import IconImg from "./IconImg";
+import { useSearchParams } from "next/navigation";
 import { Button } from "./Buttons";
 import { useMedia, useUser } from "@/pages/[id]";
 import { useCallback, useEffect, useState } from "react";
@@ -8,17 +9,33 @@ import { nha } from "@/configs/app";
 
 function FormHeader({ close }) {
   const theme = useTheme();
-  const nhaTrai = nha === 'trai'
+  const nhaTrai = nha === "trai";
   const { isPhone } = useMedia();
 
   return (
-    <Stack width='100%' padding={isPhone ? '8px 24px' : '13px 24px'} justifyContent='center' alignItems='center' borderBottom='1px solid #F5F5F5'>
-      <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: '24px' }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
-            <IconImg src="/icons/logo-chat.svg" sx={{ width: 44, height: 44 }} />
-            <Stack alignItems='center' gap='2px'>
-              <Typography variant="label" color="#202325" >
+    <Stack
+      width="100%"
+      padding={isPhone ? "8px 24px" : "13px 24px"}
+      justifyContent="center"
+      alignItems="center"
+      borderBottom="1px solid #F5F5F5"
+    >
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "flex-start", gap: "24px" }}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "12px" }}>
+            <IconImg
+              src="/icons/logo-chat.svg"
+              sx={{ width: 44, height: 44 }}
+            />
+            <Stack alignItems="center" gap="2px">
+              <Typography variant="label" color="#202325">
                 {nhaTrai ? "Việt Anh & Ngọc Diệp" : "Ngọc Diệp & Việt Anh"}
               </Typography>
             </Stack>
@@ -26,7 +43,14 @@ function FormHeader({ close }) {
         </Box>
         <IconImg
           src="/icons/minus.svg"
-          sx={{ width: 32, height: 32, cursor: 'pointer', borderRadius: '50%', backgroundColor: "#F5F5F5", '&:hover': { backgroundColor: theme.palette.neutral.midLight } }}
+          sx={{
+            width: 32,
+            height: 32,
+            cursor: "pointer",
+            borderRadius: "50%",
+            backgroundColor: "#F5F5F5",
+            "&:hover": { backgroundColor: theme.palette.neutral.midLight },
+          }}
           onClick={close}
         />
       </Box>
@@ -39,7 +63,7 @@ function FormBody() {
 
   const { user } = useUser();
   const { isPhone } = useMedia();
-  const nhaTrai = nha === 'trai';
+  const nhaTrai = nha === "trai";
 
   const [record, setRecord] = useState(null);
   const [join, setJoin] = useState(false);
@@ -54,18 +78,32 @@ function FormBody() {
 
       payload["31/12/2023"] = day.includes("31/12/2023");
       payload["01/01/2024"] = day.includes("01/01/2024");
-      console.log(payload);
-
-      const { data: { data } } = record
-        ? await axios.put(`${location.origin}/api/invitees/${user.id}`, payload, { params: { nha_trai: nhaTrai } })
-        : await axios.post(`${location.origin}/api/invitees`, { ...payload, uid: user.id }, { params: { nha_trai: nhaTrai } });
+      const {
+        data: { data },
+      } = record
+        ? await axios.put(
+            `${location.origin}/api/invitees/${user.id}`,
+            payload,
+            { params: { nha_trai: nhaTrai } }
+          )
+        : await axios.post(
+            `${location.origin}/api/invitees`,
+            { ...payload, uid: user.id },
+            { params: { nha_trai: nhaTrai } }
+          );
 
       setRecord(data.record);
     }
   }, [join, day, record, user.id, nhaTrai]);
 
-  const fetchFormData = useCallback(async() => {
-    const { data: { data: { record } } } = await axios(`${location.origin}/api/invitees/${user.id}`, { params: { nha_trai: nhaTrai } });
+  const fetchFormData = useCallback(async () => {
+    const {
+      data: {
+        data: { record },
+      },
+    } = await axios(`${location.origin}/api/invitees/${user.id}`, {
+      params: { nha_trai: nhaTrai },
+    });
 
     setRecord(record);
     setJoin(record?.join);
@@ -91,42 +129,47 @@ function FormBody() {
   }, [fetchFormData]);
 
   return (
-    <Stack alignItems='center' gap={isPhone ? '16px' : '32px'} padding={isPhone ? '8px 24px 32px' : "16px 24px 32px"} width="100%">
-      <Stack alignItems='flex-start' gap='16px' width='100%'>
-        <Stack alignItems='flex-start' gap='2px'>
+    <Stack
+      alignItems="center"
+      gap={isPhone ? "16px" : "32px"}
+      padding={isPhone ? "8px 24px 32px" : "16px 24px 32px"}
+      width="100%"
+    >
+      <Stack alignItems="flex-start" gap="16px" width="100%">
+        <Stack alignItems="flex-start" gap="2px">
           <Typography variant="title" color="#202325" textAlign="left">
             {`Xin chào ${user.username}!`}
           </Typography>
         </Stack>
-        <Stack alignItems='flex-start' gap='2px'>
-          <Typography variant="body" color="#202325" textAlign='left' >
+        <Stack alignItems="flex-start" gap="2px">
+          <Typography variant="body" color="#202325" textAlign="left">
             Vui lòng xác nhận khả năng tham dự để gia đình tiếp đón chu đáo!
           </Typography>
         </Stack>
-        <Stack alignItems='flex-start' gap='2px'>
+        <Stack alignItems="flex-start" gap="2px">
           <Typography variant="label" color="#202325">
             Khả năng tham dự:
           </Typography>
         </Stack>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
           <IconImg
             src={join ? "/icons/circle-check.svg" : "/icons/circle.svg"}
-            sx={{ cursor: 'pointer' }}
+            sx={{ cursor: "pointer" }}
             onClick={() => setJoin(true)}
           />
-          <Stack alignItems='flex-start' gap='2px'>
+          <Stack alignItems="flex-start" gap="2px">
             <Typography variant="body" color="#202325">
               Tôi sẽ tham gia
             </Typography>
           </Stack>
         </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
           <IconImg
             src={join ? "/icons/circle.svg" : "/icons/circle-check.svg"}
-            sx={{ cursor: 'pointer' }}
+            sx={{ cursor: "pointer" }}
             onClick={() => setJoin(false)}
           />
-          <Stack alignItems='flex-start' gap='2px'>
+          <Stack alignItems="flex-start" gap="2px">
             <Typography variant="body" color="#202325">
               Rất tiếc, tôi không tham gia được
             </Typography>
@@ -134,44 +177,80 @@ function FormBody() {
         </Box>
         {join && (
           <>
-            <Stack alignItems='flex-start' gap='2px'>
+            <Stack alignItems="flex-start" gap="2px">
               <Typography variant="label" color="#202325">
                 Ngày tham dự:
               </Typography>
             </Stack>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', gap: '8px' }}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                gap: "8px",
+              }}
+            >
               <IconImg
-                sx={{ cursor: 'pointer' }}
-                src={day.includes("31/12/2023") ? "/icons/rectangle-checkbox-check.svg" : "/icons/rectangle-checkbox.svg"}
-                onClick={() => setDay(prevDay => prevDay.includes("31/12/2023") ? prevDay.filter(day => day !== "31/12/2023") : [...prevDay, "31/12/2023"])}
+                sx={{ cursor: "pointer" }}
+                src={
+                  day.includes("31/12/2023")
+                    ? "/icons/rectangle-checkbox-check.svg"
+                    : "/icons/rectangle-checkbox.svg"
+                }
+                onClick={() =>
+                  setDay((prevDay) =>
+                    prevDay.includes("31/12/2023")
+                      ? prevDay.filter((day) => day !== "31/12/2023")
+                      : [...prevDay, "31/12/2023"]
+                  )
+                }
               />
-              <Stack alignItems='flex-start' gap='2px'>
+              <Stack alignItems="flex-start" gap="2px">
                 <Typography variant="body" color="#202325">
                   {nhaTrai
                     ? "Tiệc cưới, 15:00 chiều 31/12/2023"
-                    : "Tiệc cưới, 16:00 chiều 31/12/2023"
-                  }
+                    : "Tiệc cưới, 16:00 chiều 31/12/2023"}
                 </Typography>
               </Stack>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', gap: '8px' }}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                gap: "8px",
+              }}
+            >
               <IconImg
-                sx={{ cursor: 'pointer' }}
-                src={day.includes("01/01/2024") ? "/icons/rectangle-checkbox-check.svg" : "/icons/rectangle-checkbox.svg"}
-                onClick={() => setDay(prevDay => prevDay.includes("01/01/2024") ? prevDay.filter(day => day !== "01/01/2024") : [...prevDay, "01/01/2024"])}
+                sx={{ cursor: "pointer" }}
+                src={
+                  day.includes("01/01/2024")
+                    ? "/icons/rectangle-checkbox-check.svg"
+                    : "/icons/rectangle-checkbox.svg"
+                }
+                onClick={() =>
+                  setDay((prevDay) =>
+                    prevDay.includes("01/01/2024")
+                      ? prevDay.filter((day) => day !== "01/01/2024")
+                      : [...prevDay, "01/01/2024"]
+                  )
+                }
               />
-              <Stack alignItems='flex-start' gap='2px'>
+              <Stack alignItems="flex-start" gap="2px">
                 <Typography variant="body" color="#202325">
                   {nhaTrai
                     ? "Lễ thành hôn, 09:00 sáng 01/01/2024"
-                    : "Lễ vu quy, 07:00 sáng 01/01/2024"
-                  }
+                    : "Lễ vu quy, 07:00 sáng 01/01/2024"}
                 </Typography>
               </Stack>
             </Box>
           </>
         )}
-        {error && (<Typography variant="label" sx={{ color: theme.palette.error.main }}>Chưa chọn ngày tham gia</Typography>)}
+        {error && (
+          <Typography variant="label" sx={{ color: theme.palette.error.main }}>
+            Chưa chọn ngày tham gia
+          </Typography>
+        )}
       </Stack>
       <Button text="Xác nhận" type="filled" onClick={handleSubmit} />
     </Stack>
@@ -182,76 +261,110 @@ export function Form() {
   const { user } = useUser();
   const theme = useTheme();
   const { isPhone } = useMedia();
-
+  const searchParams = useSearchParams();
   const [open, setOpen] = useState(false);
-  const [pos, setPos] = useState(isPhone ? { left: 220, top: 600 } : { left: 850, top: 680 });
-
+  const [pos, setPos] = useState(
+    isPhone ? { left: 220, top: 600 } : { left: 850, top: 680 }
+  );
+  const form = searchParams.get("form");
+  console.log(form);
   const handleDrag = (event) => {
     event.preventDefault();
     const { clientX, clientY } = event;
 
     if (!clientX || !clientY) return;
     setPos({ left: clientX, top: clientY });
-  }
+  };
 
   useEffect(() => {
     setPos(isPhone ? { left: 220, top: 600 } : { left: 850, top: 680 });
   }, [isPhone]);
 
-  return (
+  return form === "true" ? (
     <>
       {isPhone && open && (
         // <Box sx={{ width: '100vw', height: '100vh' }}>
-          <Box sx={{
-            display: 'inline-flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            borderRadius: '20px',
-            border: '1px solid #F5F5F5',
+        <Box
+          sx={{
+            display: "inline-flex",
+            flexDirection: "column",
+            alignItems: "center",
+            borderRadius: "20px",
+            border: "1px solid #F5F5F5",
             background: theme.palette.neutral.light,
             width: isPhone ? 328 : 375,
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
             zIndex: 1010,
-            boxShadow: '0px 0px 10px 0px rgba(88, 131, 191, 0.50)',
-          }}>
-            <FormHeader close={() => setOpen(false)} />
-            <FormBody user={user} />
-          </Box>
+            boxShadow: "0px 0px 10px 0px rgba(88, 131, 191, 0.50)",
+          }}
+        >
+          <FormHeader close={() => setOpen(false)} />
+          <FormBody user={user} />
+        </Box>
         // </Box>
       )}
-      <Box sx={{ position: 'fixed', ...pos, zIndex: 1005 }} onDrag={handleDrag} draggable="true" >
+      <Box
+        sx={{ position: "fixed", ...pos, zIndex: 1005 }}
+        onDrag={handleDrag}
+        draggable="true"
+      >
         {!isPhone && open && (
-          <Box sx={{
-            display: 'inline-flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            borderRadius: '20px',
-            border: '1px solid #F5F5F5',
-            background: theme.palette.neutral.light,
-            width: isPhone ? 328 : 375,
-            ...(isPhone ? {
-              top: '50vh',
-              left: '50vw',
-              transform: 'translate(-50%, -50%)',
-            } : {
-              position: 'absolute',
-              transform: 'translate(30px, -100%)',
-            })
-          }}>
+          <Box
+            sx={{
+              display: "inline-flex",
+              flexDirection: "column",
+              alignItems: "center",
+              borderRadius: "20px",
+              border: "1px solid #F5F5F5",
+              background: theme.palette.neutral.light,
+              width: isPhone ? 328 : 375,
+              ...(isPhone
+                ? {
+                    top: "50vh",
+                    left: "50vw",
+                    transform: "translate(-50%, -50%)",
+                  }
+                : {
+                    position: "absolute",
+                    transform: "translate(30px, -100%)",
+                  }),
+            }}
+          >
             <FormHeader close={() => setOpen(false)} />
             <FormBody user={user} />
           </Box>
         )}
-        <Box sx={{ display: 'flex', padding: '16px 24px', gap: '16px', justifyContent: 'center', alignItems: 'center' }}>
-          <Box padding='8px 16px' borderRadius='24px' border={`1px solid ${theme.palette.neutral.midLight}`} sx={{ background: `${theme.palette.neutral.light}` }}>
-            <Typography variant="body" color={theme.palette.neutral.dark}>Xác nhận khả năng tham gia của bạn!</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            padding: "16px 24px",
+            gap: "16px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            padding="8px 16px"
+            borderRadius="24px"
+            border={`1px solid ${theme.palette.neutral.midLight}`}
+            sx={{ background: `${theme.palette.neutral.light}` }}
+          >
+            <Typography variant="body" color={theme.palette.neutral.dark}>
+              Xác nhận khả năng tham gia của bạn!
+            </Typography>
           </Box>
-          <IconImg width={60} height={60} src="/icons/chatbot.svg" onClick={() => setOpen(prev => !prev)} sx={{ cursor: 'pointer' }} />
+          <IconImg
+            width={60}
+            height={60}
+            src="/icons/chatbot.svg"
+            onClick={() => setOpen((prev) => !prev)}
+            sx={{ cursor: "pointer" }}
+          />
         </Box>
       </Box>
     </>
-  );
+  ) : null;
 }
