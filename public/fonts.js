448ae658b89e25dbbb0a0c1af1e7f619f28a7798
PhamManh2911 <phammanh2911@gmail.com>
2024-01-03 17:50:41 +0700

    refactor: using woff2 font family format

--- public/fonts.js
+++ public/fonts.js
@@ -2,32 +2,32 @@ import localFont from "next/font/local";
 
 export const serif = localFont({
   src: [
-    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Black.ttf" },
-    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-BlackItalic.ttf" },
-    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Bold.ttf" },
-    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-BoldItalic.ttf" },
-    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Italic.ttf" },
-    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Regular.ttf" },
+    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Black.woff2" },
+    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-BlackItalic.woff2" },
+    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Bold.woff2" },
+    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-BoldItalic.woff2" },
+    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Italic.woff2" },
+    { path:"./fonts/Playfair_Display_SC/PlayfairDisplaySC-Regular.woff2" },
   ],
   variable: "--font-serif",
 });
 
 export const sansserif = localFont({
   src: [
-    { path: "./fonts/Inter/Inter-Black.ttf" },
-    { path: "./fonts/Inter/Inter-Bold.ttf" },
-    { path: "./fonts/Inter/Inter-ExtraBold.ttf" },
-    { path: "./fonts/Inter/Inter-ExtraLight.ttf" },
-    { path: "./fonts/Inter/Inter-Light.ttf" },
-    { path: "./fonts/Inter/Inter-Medium.ttf" },
-    { path: "./fonts/Inter/Inter-Regular.ttf" },
-    { path: "./fonts/Inter/Inter-SemiBold.ttf" },
-    { path: "./fonts/Inter/Inter-Thin.ttf" },
-    { path: "./fonts/Inter/Inter-VariableFont_slnt,wght.ttf" },
+    { path: "./fonts/Inter/Inter-Black.woff2" },
+    { path: "./fonts/Inter/Inter-Bold.woff2" },
+    { path: "./fonts/Inter/Inter-ExtraBold.woff2" },
+    { path: "./fonts/Inter/Inter-ExtraLight.woff2" },
+    { path: "./fonts/Inter/Inter-Light.woff2" },
+    { path: "./fonts/Inter/Inter-Medium.woff2" },
+    { path: "./fonts/Inter/Inter-Regular.woff2" },
+    { path: "./fonts/Inter/Inter-SemiBold.woff2" },
+    { path: "./fonts/Inter/Inter-Thin.woff2" },
+    { path: "./fonts/Inter/Inter-VariableFont_slnt,wght.woff2" },
   ],
   variable: "--font-sans-serif",
 });
 
 export const bellissima = localFont({
-  src: "./fonts/HLT BellissimaScriptPro.ttf",
+  src: "./fonts/HLT BellissimaScriptPro.woff2",
 });
